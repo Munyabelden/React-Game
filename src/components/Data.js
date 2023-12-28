acb5b8e13542092223f8e0f2929db27922a4eb75
munyamugb <munyaradzi045@gmail.com>
2023-12-28 13:51:06 +0200

    Add game functionality

--- src/components/Data.js
+++
@@ -1,9 +0,0 @@
-import rock from './image/icon-rock.svg';
-import paper from './image/icon-paper.svg';
-import scissors from './image/icon-scissors.svg';
-import lizard from './image/icon-lizard.svg';
-import spock from './image/icon-spock.svg';
-
-const data = [rock, paper, scissors, lizard, spock];
-
-export default data;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,36 +1,80 @@
 import React, { useState } from 'react';
-import data from './Data';
+import rock from './image/icon-rock.svg';
+import lizard from './image/icon-lizard.svg';
+import paper from './image/icon-paper.svg';
+import scissors from './image/icon-scissors.svg';
+import spock from './image/icon-spock.svg'; 
 
-export default function Game() {
-  const [ score, setScore] = useState(0);
+const images = [
+  { src: rock, alt: 'Rock' },
+  { src: paper, alt: 'Paper' },
+  { src: scissors, alt: 'Scissors' },
+  { src: lizard, alt: 'Lizard' },
+  { src: spock, alt: 'Spock' },
+];
 
-  const displayImages = () => {
-    // eslint-disable-next-line array-callback-return
-    return data.map((image, index) => {
-      <li key={index}>
-        <img src={image} alt={image}/>
-      </li>
-    })
+const Game = () => {
+  const [score, setScore] = useState(0);
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+
+  const determineWinner = (user, computer) => {
+    // Game rules logic goes here, you can define your own rules
+    // For simplicity, let's say Paper beats Rock, Rock beats Scissors, and Scissors beats Paper
+    // Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, Rock crushes Scissors
+    // In case of a draw, no score changes
+    // Implement your own game rules here
+
+    if (user === computer) {
+      return 'It\'s a tie!';
+    } else if (
+      (user === 'Rock' && (computer === 'Scissors' || computer === 'Lizard')) ||
+      (user === 'Paper' && (computer === 'Rock' || computer === 'Spock')) ||
+      (user === 'Scissors' && (computer === 'Paper' || computer === 'Lizard')) ||
+      (user === 'Lizard' && (computer === 'Spock' || computer === 'Paper')) ||
+      (user === 'Spock' && (computer === 'Scissors' || computer === 'Rock'))
+    ) {
+      setScore(score + 1);
+      return 'You win!';
+    } else {
+      setScore(score - 1);
+      return 'You lose!';
+    }
+  };
+
+  const handleButtonClick = (userChoiceIndex) => {
+    const userSelected = images[userChoiceIndex].alt;
+    setUserChoice(images[userChoiceIndex].src);
+
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setComputerChoice(images[randomIndex].src);
+
+    const result = determineWinner(userSelected, images[randomIndex].alt);
+    alert(result);
   };
 
   return (
     <div>
+      <div>
+        Score: {score}
+      </div>
       <div className="score-board">
         <ul>
-          <li>Rock</li>
-          <li>Paper</li>
-          <li>Scissors</li>
-          <li>Lizard</li>
-          <li>Spock</li>
+          {images.map((image, index) => (
+            <li key={index}>
+              <button type="button" onClick={() => handleButtonClick(index)}>
+                <img src={image.src} alt={image.alt} />
+              </button>
+            </li>
+          ))}
         </ul>
-        <div>
-          Score
-          {score} 
-        </div>
       </div>
-      <ul>
-        {displayImages()}
-      </ul>
+      <div>
+        {userChoice && <p>Your choice: <img src={userChoice} alt="User choice" /></p>}
+        {computerChoice && <p>Computer's choice: <img src={computerChoice} alt="Computer choice" /></p>}
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Game;
